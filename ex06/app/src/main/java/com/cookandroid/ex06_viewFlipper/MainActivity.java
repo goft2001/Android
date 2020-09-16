5b7907866afe0f2b5e79909c8fb636b0162882c8
Android <igseo92@naver.com>
2020-09-16 16:19:36 +0900

    android_Day03

---
+++ ex06/app/src/main/java/com/cookandroid/ex06_viewFlipper/MainActivity.java
@@ -0,0 +1,45 @@
+package com.cookandroid.ex06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnPrev,btnNext;
+        final ViewFlipper vFlipper;
+
+        btnPrev = (Button)findViewById(R.id.btnPrev);
+        btnNext = (Button)findViewById(R.id.btnNext);
+
+        vFlipper = (ViewFlipper)findViewById((R.id.viewFilpper1));
+
+
+
+        btnPrev.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vFlipper.startFlipping();
+                vFlipper.setFlipInterval(1000);
+            }
+        }));
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vFlipper.stopFlipping();
+            }
+        });
+
+
+
+    }
+}
